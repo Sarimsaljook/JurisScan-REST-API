54e883ff061ca6c36b5df9abbce1b415c73a21a5
Sarimsaljook <80625774+Sarimsaljook@users.noreply.github.com>
2024-07-04 20:28:49 -0400

    Add File Path Prop to User Files Table

--- JurisScan_REST_API/settings.py
+++ JurisScan_REST_API/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -127,3 +128,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+django_heroku.settings(locals())
--- JurisScan_REST_API/views.py
+++ JurisScan_REST_API/views.py
@@ -3,24 +3,23 @@ import base64
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.core.exceptions import ObjectDoesNotExist
 from django.db import connection
-from .models import UserFile
-from .serializers import UserFileSerializer
-
 
 class UploadFileView(APIView):
     def post(self, request):
         user_id = request.data.get('user_id')
         file = request.data.get('file')
+        file_path = request.data.get('file_path')
         if not user_id or not file:
             return Response({'error': 'user_id and file are required'}, status=status.HTTP_400_BAD_REQUEST)
 
         # Save file to user's table
         table_name = f'user_{user_id}'
         with connection.cursor() as cursor:
-            cursor.execute(f"CREATE TABLE IF NOT EXISTS {table_name} (file_name VARCHAR(255), file LONGBLOB)")
-            cursor.execute(f"INSERT INTO {table_name} (file_name, file) VALUES (%s, %s)", [file.name, file.read()])
+            cursor.execute(
+                f"CREATE TABLE IF NOT EXISTS {table_name} (file_name VARCHAR(255), file_path VARCHAR(255), file LONGBLOB)")
+            cursor.execute(f"INSERT INTO {table_name} (file_name, file_path, file) VALUES (%s, %s, %s)",
+                           [file.name, file_path, file.read()])
 
         return Response({'message': 'File uploaded successfully'}, status=status.HTTP_201_CREATED)
 
@@ -33,10 +32,11 @@ class GetUserFilesView(APIView):
             if cursor.fetchone() is None:
                 return Response({'error': 'User table does not exist'}, status=status.HTTP_404_NOT_FOUND)
 
-            cursor.execute(f"SELECT file_name, file FROM {table_name}")
+            cursor.execute(f"SELECT file_name, file_path, file FROM {table_name}")
             files = cursor.fetchall()
 
-            response_files = [{'file_name': file[0], 'file_content': base64.b64encode(file[1]).decode('utf-8')} for file in files]
+            response_files = [
+                {'file_name': file[0], 'file_path': file[1], 'file_content': base64.b64encode(file[2]).decode('utf-8')}
+                for file in files]
 
             return Response({'files': response_files}, status=status.HTTP_200_OK)
-
